5df5f6de62bd253a9516bbf024deae81161f4154
Keith Rogers <git@keithrogers.eu>
2018-11-22 18:25:20 +0000

    Add campaign metrics endpoint
     🐿 v2.11.0

--- tests/metrics.test.js
+++ tests/metrics.test.js
@@ -29,6 +29,192 @@ describe('Fetch ESP deliverability metrics:', () => {
 		nock.enableNetConnect();
 	});
 
+	describe('Campaign endpoint', () => {
+		const campaignFixtures = {
+			ok: require('./fixtures/metrics-discoverability-campaign-200.json'),
+			badRequest: require('./fixtures/metrics-discoverability-campaign-400.json'),
+			serverError: require('./fixtures/metrics-discoverability-campaign-500.json')
+		};
+
+		const exampleMetrics = 'metrics=count_targeted,count_delivered,count_unique_confirmed_opened';
+		const campaignUrl = `/deliverability/campaign?${defaultQueryParams.join('&')}&${exampleMetrics}`;
+		const goodParams = {
+			apiName: 'campaign',
+			metricNames: new Set(['count_targeted', 'count_delivered', 'count_unique_confirmed_opened'])
+		};
+
+		describe('When everything is great', () => {
+			beforeEach(() => {
+				this.nocked = nock(espMetricsBaseUrl)
+					.get(campaignUrl)
+					.reply(200, campaignFixtures.ok);
+			});
+			afterEach(nock.cleanAll);
+
+			describe('Promises', () => {
+				it('returns something thenable', () => {
+					expect(fetchMetrics(goodParams).then).to.be.a('function');
+				});
+
+				it('returns a resolved promise', (done) => {
+					fetchMetrics(goodParams)
+						.then(() => done())
+						.catch(done);
+				});
+			});
+
+			describe('Fetch requests', () => {
+				it('calls the expected url', () => {
+					return fetchMetrics(goodParams).then(() => {
+						expect(this.nocked.isDone()).to.be.true;
+					});
+				});
+
+				it('calls with the expected `Authorization` header', () => {
+					this.nocked.matchHeader('Authorization', testEspMetricsApiKey);
+
+					return fetchMetrics(goodParams).then(() => {
+						expect(this.nocked.isDone()).to.be.true;
+					});
+				});
+
+				it('parses a JSON string response', () => {
+					nock.cleanAll();
+					nock(espMetricsBaseUrl)
+						.get(campaignUrl)
+						.reply(200, JSON.stringify({ results: { test: 'hello' } }));
+
+					return fetchMetrics(goodParams).then((metrics) => {
+						expect(metrics).to.deep.equal({ test: 'hello' });
+					});
+				});
+
+				it('passes on a custom `from` ISO datetime', () => {
+					nock.cleanAll();
+
+					const customFrom = '2018-01-01T10:00';
+					const [, defaultTimezone] = defaultQueryParams;
+					const campaignUrlCustomFrom = `/deliverability/campaign?from=${customFrom}&${defaultTimezone}&${exampleMetrics}`;
+
+					const nocked = nock(espMetricsBaseUrl)
+						.get(campaignUrlCustomFrom)
+						.reply(200, campaignFixtures.ok);
+
+					const params = {
+						...goodParams,
+						fromIsoDatetime: customFrom
+					};
+
+					return fetchMetrics(params).then(() => {
+						expect(nocked.isDone()).to.be.true;
+					});
+				});
+
+				it('passes on requested metrics names to the API', () => {
+					nock.cleanAll();
+
+					const [defaultFrom, defaultTimezone] = defaultQueryParams;
+					const customMetrics = ['count_delayed', 'count_unsubscribe'];
+					const campaignUrlCustomMetrics = `/deliverability/campaign?${defaultFrom}&${defaultTimezone}&metrics=${customMetrics.join()}`;
+
+					const nocked = nock(espMetricsBaseUrl)
+						.get(campaignUrlCustomMetrics)
+						.reply(200, campaignFixtures.ok);
+
+					const params = {
+						...goodParams,
+						metricNames: new Set(customMetrics)
+					};
+
+					return fetchMetrics(params).then(() => {
+						expect(nocked.isDone()).to.be.true;
+					});
+				});
+
+				it('passes on requested orderBy field to the API', () => {
+					nock.cleanAll();
+
+					const [defaultFrom, defaultTimezone] = defaultQueryParams;
+					const campaignUrlCustomOrder = `/deliverability/campaign?${defaultFrom}&${defaultTimezone}&${exampleMetrics}&order_by=count_targeted`;
+
+					const nocked = nock(espMetricsBaseUrl)
+						.get(campaignUrlCustomOrder)
+						.reply(200, campaignFixtures.ok);
+
+					const params = {
+						...goodParams,
+						orderBy: 'count_targeted'
+					};
+
+					return fetchMetrics(params).then(() => {
+						expect(nocked.isDone()).to.be.true;
+					});
+				});
+			});
+		});
+
+		describe('When not everything is great tbqh', () => {
+			afterEach(nock.cleanAll);
+
+			it('rejects if no apiName is passed in', () => {
+				const promise = fetchMetrics({
+					...goodParams,
+					apiName: null
+				});
+
+				return expect(promise).to.be.rejectedWith(Error);
+			});
+
+			it('rejects if no metricNames are passed in', () => {
+				const promise = fetchMetrics({
+					...goodParams,
+					metricNames: null
+				});
+
+				return expect(promise).to.be.rejectedWith(Error);
+			});
+
+			it('rejects if an unknown apiName is passed in', () => {
+				const promise = fetchMetrics({
+					...goodParams,
+					apiName: 'totallyMadeUpApiName'
+				});
+
+				return expect(promise).to.be.rejectedWith(Error);
+			});
+
+			it('rejects if the server responds with an error (how dare they)', () => {
+				nock(espMetricsBaseUrl)
+					.get(campaignUrl)
+					.reply(500, campaignFixtures.serverError);
+
+				const promise = fetchMetrics(goodParams);
+
+				return expect(promise).to.be.rejectedWith(Error);
+			});
+
+			it('rejects if the server responds with a bad request error', () => {
+				nock(espMetricsBaseUrl)
+					.get(campaignUrl)
+					.reply(400, campaignFixtures.badRequest);
+
+				const promise = fetchMetrics(goodParams);
+
+				return expect(promise).to.be.rejectedWith(Error);
+			});
+
+			it('rejects if the server responds as a teapot', () => {
+				nock(espMetricsBaseUrl)
+					.get(campaignUrl)
+					.reply(418, campaignFixtures.serverError);
+
+				const promise = fetchMetrics(goodParams);
+
+				return expect(promise).to.be.rejectedWith(Error);
+			});
+		});
+	});
+
 	describe('IP pool endpoint', () => {
 		const ipPoolFixtures = {
 			ok: require('./fixtures/metrics-discoverability-ip-pool-200.json'),
