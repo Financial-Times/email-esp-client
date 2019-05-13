9f37ed4f928173f909636ba5dcfce59d7e0778a5
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2019-05-13 17:22:46 +0100

    Fix vuls. 🐿 v2.12.3

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -1,11 +1,102 @@
 module.exports = {
 	files: {
-		allow: [],
+		allow: [
+			'.github/CODEOWNERS'
+		],
 		allowOverrides: []
 	},
 	strings: {
 		deny: [],
 		denyOverrides: [
+			'\\+XFKjKaN9iVmS84dPwVN00W4wPmqvYoZF3EGAsfw', // package-lock.json:19
+			'/OubMVb6lUPMagqjOPnmtauXnFCACVl3O7ogjeqQ', // package-lock.json:86
+			'ay64MW0Zo8aJ8gZPWMMK6', // package-lock.json:86
+			'c35YEBTkL4rzXY2ucpSKy', // package-lock.json:108
+			'UvEQPCo6eiwzzSaSWfpaDhOmStMCMod6wmuPciNq', // package-lock.json:123|123
+			'kyEyQEagBC5mBEFlIYvdg', // package-lock.json:210
+			'TNPjfTr432qx7yOjQyaXm3dSR0MH9vXp7eT1BFSl', // package-lock.json:267|267
+			'qhAVI1\\+Av2X7qelOfAIYwXONood6XlZE/fXaBSmW', // package-lock.json:297
+			'/5L0HNZzGQo4fuSPnK\\+wjfPnKZV0aiJDgzmWqqkV', // package-lock.json:356
+			'wjfPnKZV0aiJDgzmWqqkV', // package-lock.json:356
+			'/xjLqNMkv3BPPp2JyyuTHOrf9BOosGpNQ11v6BKv', // package-lock.json:380
+			'WclKAEzWH47lCdplFocUM', // package-lock.json:422
+			'\\+if6uywV0nDGoiydJRy4yk7h9od5Og0kxx4zUXmw', // package-lock.json:541
+			'\\+EYGZDY/HCWC\\+iAEA9h1ByhMXAIVUXMcMFpOCxQ/', // package-lock.json:577|1900
+			'eTVLrBSt7fjbDygz805pMnstIs2VTBNkRm0qxZd\\+', // package-lock.json:607
+			'hYD5i0aPN5QwZisEbDStI', // package-lock.json:624
+			'OX8XqP7/1a9cqkxYw2yXss15f26NKWBpDXQd0/uK', // package-lock.json:645
+			'\\+3Uk5DYh6/1eKO0m0YmJFGNmFHGACpf1ClL1nmlV', // package-lock.json:658
+			'\\+C10TsW4PURY/ic\\+eaysnSkwB4kA/mBlCyy/IKDJ', // package-lock.json:742
+			'/L22nqrSY5DKlo3X6\\+vclJm8Bb5djXJBmEX6fS3\\+', // package-lock.json:777
+			'vclJm8Bb5djXJBmEX6fS3', // package-lock.json:777
+			'\\+icEBfc0U7qJCisqrTsKTjw4fNFy2pW9OqStD84g', // package-lock.json:785
+			'LaxLTV6UC//WewneB3LcXOL9NP\\+\\+ozKVXsIIIpm/', // package-lock.json:807
+			'cLu3NgCM3AHu1CkJ5Hzt9V0Y0PBAO6Ay', // package-lock.json:855
+			'EfhWI4gl1Hwq8B/GmY/0oXZ6nF8hDVesS/FpnYaD', // package-lock.json:910|1300|2278|2363|3327|3496
+			'E7AdgFBVeAPVMNcKGsHMA', // package-lock.json:939|960|989
+			'AY6sXByma2Wr0TXvXJ4nA', // package-lock.json:971
+			'BOUkdmRmA9riiIPVvo5x86m5elviOk0Q', // package-lock.json:1032
+			'FHyH341ZrbnMUpe\\+5Bocte9xkmFMzPMjRaZMcXww', // package-lock.json:1038
+			'SVeJBDM/gCtMARO0cLuT2HcEKnTPvhjV6aGeqrCB', // package-lock.json:1055
+			'gCtMARO0cLuT2HcEKnTPvhjV6aGeqrCB', // package-lock.json:1055
+			'\\+A9DVCndXfkeFUd3byderg\\+EbDkfnevfCwynWaNA', // package-lock.json:1060
+			'A9DVCndXfkeFUd3byderg', // package-lock.json:1060
+			'SRUReLS5Q8a7GxtRdxEBVZpm98rJM7Sb', // package-lock.json:1060
+			'nxfAdhnox83seTDbwnaqKO4sXRy7roHAIFqJP/Rw', // package-lock.json:1088
+			'qWoOQKyZDo25mOMVpSmXROAsTJVVVYzrrtol3d4y', // package-lock.json:1107|1107
+			'5y4gQJQzoGY2YCPdaIekE', // package-lock.json:1259
+			'x5j6Ks7FOgLD/GlvjKwgu7wdmMR55iuRHhn8hj/\\+', // package-lock.json:1279
+			'kiA9Or1bgZhQFNppJX6h/f5McrvJt1mQXTFm6Qrw', // package-lock.json:1315
+			'JAzQV4WpoY5WHcG0S0HHY', // package-lock.json:1360
+			'TyI9jIy2J4j0qgPmOOrHTCtpPqJGN/aurBwc6ZT\\+', // package-lock.json:1390
+			'iR0jqwsOrOxMZyGYqUuDhJ0l4AXdO1iX', // package-lock.json:1507
+			'h7cYS30Z5\\+R7WaiCCBfLq58ZI/dSeaEKb9WVJV5n', // package-lock.json:1572
+			'YdcWFniJhzI42\\+AzS\\+gNwmUzOSFcRCQYwySuBBBy', // package-lock.json:1602
+			'PaQqUC9SRmAiSA9CCCYd4', // package-lock.json:1659
+			'Df2Y9akRFxbdU13aZJL2e', // package-lock.json:1757
+			'qgDYXFSR5WvEfuS5dMj6oTMEbrrSaM0CrFk2Yiq/', // package-lock.json:1811
+			'0eRIASHZt1E68/ixClI8bp2YK2wmBPVWEismTs6M', // package-lock.json:1822
+			'TSfVAu49jYC4BvQ4Sms9SZgdqGBgroqfDhJdTyKQ', // package-lock.json:1878|1878
+			'sWZlbEP2OsHNkXrMl5GYk', // package-lock.json:2121
+			'/ad/2oNbKtW9Hgovl5O1FvFWKkKblNXoN/Oou6\\+9', // package-lock.json:2376
+			'JJi7duF1o\\+u2pynbp2zXDW2/PADgC30f0GsHZtRh', // package-lock.json:2388
+			'PqHpggC9bLV0VeWcdKhkpxY\\+3JTzetLSqTCWL/z/', // package-lock.json:2463
+			'\\+Z/4UeDaLuSW\\+39JPeFgs4gCGqsrJHVZX0fUrx//', // package-lock.json:2713
+			'\\+K6LxJrX5UPyKFvGnL5CYmq2jFGNIBWHpc4QyE49', // package-lock.json:2734
+			'\\+p3SFs1Pytd/jYct4wpv49HiYCqd9Rlc5ZVdk0V\\+', // package-lock.json:2789
+			'wnvdQcjq9TZjevvXzSUo7bfmw91saqMjzGS2xq91', // package-lock.json:2803|2803
+			'/iJIi40O5cGV1wNYp5hvZP8ZN0T\\+jiNkL0QepXs\\+', // package-lock.json:2808
+			'BTL5aDJVBMkss0dthToPw', // package-lock.json:2869
+			'lv0M6\\+TkDVniA3aD1Eg0DVpfU/booSu7Eev3TDO/', // package-lock.json:2889
+			'AicPrAC7Qu1JxPCZ9ZgCZlY35QgFnNqc', // package-lock.json:2949
+			'6BctqTVQiBP3YUJ9C6DQOXJmkYR9X9fCLtCOJc5w', // package-lock.json:2973|2973
+			'JTWmoY9tWCs7zvIk/CvRjhjGaOd\\+OVBM987mxFo\\+', // package-lock.json:2996
+			'Gd2UZBJDkXlY7GbJxfsE8', // package-lock.json:3005
+			'JvdAWfbXeIGaZ9cILp38HntZSFSo3mWg6xGcJJsd', // package-lock.json:3049|3049
+			'iYagtjLVt1vN3zZY7D8oH7dkjNJEjLjyuzy8daX5', // package-lock.json:3093|3093
+			'ZEWEcW4SfFNTr4uMNZma0ey4f5lgLrkB0aX0QMow', // package-lock.json:3110|3110
+			'JDhEpTKzXusOqXZ0BUIdH', // package-lock.json:3136
+			'rmMueb7tWZjQ1rx4Loy1ArBggoqGpfqqdI4rondg', // package-lock.json:3195|3195
+			'1apePfXM1UOSqw0o9IiFAovVz9M5S1Dg', // package-lock.json:3195
+			'f8FqlyAdouralcFWFQWF2', // package-lock.json:3200|3345
+			'cBhpre4ma\\+U0T1oM5fXg7Dy1Jw7zzwv7lt/GoCpr', // package-lock.json:3248
+			'9rFapKFnlbirLi3byoOuhrSsli/z/ihNnvM24vgF', // package-lock.json:3263|3442
+			'\\+mA30U0DTotfDZ0d2UUsXpPmPmMMJ6W773MaA3J\\+', // package-lock.json:3268
+			'j//nPX8wTSZUPF8QeRNEk/sX\\+6df7M1y6PBHGSHA', // package-lock.json:3357
+			'vxfLAoZ7mDsNXgjPn5M4ZEaeHFCeWcyA', // package-lock.json:3388
+			'26QrsUIAtLQ2eHl3LfAXGTp6131tWyGw', // package-lock.json:3408
+			'\\+/Vmv6tcXqAEotstW/q24EBC3wmnRyAxzfZ7qTMQ', // package-lock.json:3449
+			'\\+RYJU8a67zmJ8WFFPvSxyCjdlKIcsps4hPQFebhz', // package-lock.json:3457
+			'QH9pZDd04d9W9aofGvON8NJeOn9KS39Q', // package-lock.json:3503
+			'/h0Plj6enJqjz1Zbq2l5WaqYnrVbwWOWMyF3F47g', // package-lock.json:3545
+			'sEkzewTm\\+\\+BPi7Hvn/HwcqLAcNxYAyI0x13CpPPn', // package-lock.json:3550
+			'q9hedtzyXHr5S0A1vEPoK', // package-lock.json:3559
+			'/0sO6neh2jqRDVHOQ4o/LMea0tgCkbMgea5ip/e\\+', // package-lock.json:3575
+			'enyl4ETZAfTaeksa6BXks5ZcjpSUNrjBr0DvrJKX', // package-lock.json:3596|3596
+			'/4vDM54WJsJio3XNn6K2sCG\\+CQ8G5Wz6bZhRZoAe', // package-lock.json:3780
+			'YTdrdZheS8TFwvkbLWf/G5KNJDCh6pKL5OZctEW4', // package-lock.json:3883
+			'LbqTKRAQ5kw8A90rA6fr4riOUpTZvQZA', // package-lock.json:3904
+			'JmbhsDo7fcAJq4s9h27Ew', // package-lock.json:3909
 			'05c3dad0-0e07-4fb5-8956-e548a5490987', // tests/fixtures/metrics-discoverability-campaign-200.json:7
 			'444eece0-54b3-49f3-b9fa-afc2bc7c81c5', // tests/fixtures/metrics-discoverability-campaign-200.json:13
 			'8a97f29e-27da-4e05-832a-574837257ed3', // tests/fixtures/metrics-discoverability-campaign-200.json:19
